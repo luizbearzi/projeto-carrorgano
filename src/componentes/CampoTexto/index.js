c97b3045b391a228ee21f7c774cb86a7a1dad73d
luizbearzi <94871271+luizbearzi@users.noreply.github.com>
2023-07-06 14:15:31 -0300

    finalizado interagindo com o usuário e começar o montando times
    começar 04 - montando times

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -2,18 +2,16 @@ import './CampoTexto.css'
 
 const CampoTexto = (props) => {
     const placeholderModificada = `${props.placeholder}...`
-    let valor = 'luiz'
 
-const aoDigitado = (evento) => {   
-    valor = evento.target.value
-    console.log(valor)
+    const aoDigitado = (evento) => {   
+    props.aoAlterado(evento.target.value)
     
-}
+    }
 
     return (
         <div className='campo-texto'>
             <label>{props.label}</label>
-            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificada}/>
         </div>
     )
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,9 +1,10 @@
+import { useState } from 'react'
 import Botao from '../Botao'
 import CampoTexto from '../CampoTexto'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
 
-const Formulario = () => {
+const Formulario = (props) => {
 
      const times = [
         'Corrida',
@@ -12,19 +13,52 @@ const Formulario = () => {
         'Trabalho'
      ]
 
+     const [carro, setCarro] = useState('')
+     const [marca, setMarca] = useState('')
+     const [imagem, setImagem] = useState('')
+     const [time, setTime] = useState('')
+
      const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('Form foi submetido')
+        props.aoColaboradorCadastrado ({
+            carro,
+            marca,
+            imagem,
+            time
+        })
      }
 
     return(
         <section className='formulario'>
                 <form onSubmit={aoSalvar}>
                     <h2>Preencha os dados para criar o card do Carro</h2>
-                    <CampoTexto obrigatorio={true} label ="Carro" placeholder="Informe o carro"/>
-                    <CampoTexto obrigatorio={true} label="Marca" placeholder="Informe a marca"/>
-                    <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-                    <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                    <CampoTexto 
+                    obrigatorio={true} 
+                    label ="Carro" 
+                    placeholder="Informe o carro"
+                    valor={carro}
+                    aoAlterado={valor => setCarro(valor)}
+                    />
+                    <CampoTexto 
+                    obrigatorio={true} 
+                    label="Marca" 
+                    placeholder="Informe a marca"
+                    valor={marca}
+                    aoAlterado={valor => setMarca(valor)}
+                    />
+                    <CampoTexto 
+                    label="Imagem" 
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                    />
+                    <ListaSuspensa 
+                    obrigatorio={true} 
+                    label="Time" 
+                    itens={times}
+                    valor={time}
+                    aoAlterado={valor => setTime(valor)}
+                    />
                     <Botao>
                         Criar Card
                     </Botao>
